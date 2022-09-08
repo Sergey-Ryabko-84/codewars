fb30e39135ef42eaf035cc2ebf6dac45e5dd12ef
Sergey-Ryabko-84 <amigoswow@ukr.net>
2022-09-08 21:02:08 +0300

    solved-the-first-task

---
+++ js/script.js
@@ -0,0 +1,31 @@
+// CODEWARS
+
+// 2022.09.08
+
+// Multiples of 3 or 5
+
+// If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+// Finish the solution so that it returns the sum of all the multiples of 3 or 5 below the number passed in. Additionally, if the number is negative, return 0 (for languages that do have them).
+// Note: If the number is a multiple of both 3 and 5, only count it once.
+// Courtesy of projecteuler.net (Problem 1)
+
+// -----------------------------Solution------------------------------
+function solution(number){
+    if (number < 0) {
+      return 0;
+    }
+    
+    let total = 0;
+    
+    for (let i = 0; i < number; i++ ) {
+        i % 3 === 0 || i % 5 === 0 ? total += i : null;
+    }
+    
+    return total;
+  }
+
+  console.log(solution(10));
+
+// *******************************************************************
+
+
